5878b76c3b0dd0f5d9c2c71a9bb2c167164e50be
Rishabh Jain <rishabhjainfinal@gmail.com>
2023-07-12 23:45:01 +0530

    added new functionality
    create using providing data and update the data in the middle

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 # create resumes on different templates
-import os 
-from constants import outputDir
+import json
 from resumeFormat.twoColumn import runner as twoColumnRunner
 from resumeFormat.singleColumn import runner as singleColumnRunner
+a
+if __name__ == '__main__':
+    filename1 = '1ColumnResume.pdf'
+    filename2 = '2ColumnResume.pdf'
 
+    # read the json file template.json
+    with open('template.json') as f:
+        data = json.load(f)
 
-# first check for possible updates from linkedin and update the json file
-# update the json file with latest linkedin data 
-# update the json file with latest github data 
-
-filename1 = '1ColumnResume.pdf'
-filename2 = '2ColumnResume.pdf'
-
-# 1 column resume
-singleColumnRunner(filename1)
-
-# 2 column resume
-twoColumnRunner(filename2)
-
-# os.system(f'open {os.path.join(outputDir,filename2)}')
+    # testing for updating the json data in the middle
+    data['basics']['name'] = 'Rahul'
+    filePath = singleColumnRunner(filename1, data)
+    print(filePath)
+    filePath2 = twoColumnRunner(filename2, data)
+    print(filePath2)
