661d4e934230a1a1572901b3845f1ed2844b4ee3
Rishabh Jain <rishabhjainfinal@gmail.com>
2023-07-20 17:58:23 +0530

    updates for server small

--- scripts/config/certbotConfig.sh
+++ scripts/config/certbotConfig.sh
@@ -1,4 +1,6 @@
 
 # sudo add-apt-repository ppa:certbot/certbot > /dev/null
-sudo apt install certbot python3-certbot-nginx -y > /dev/null 
-sudo certbot --nginx -d $websiteUrl -m $email --agree-tos --non-interactive
+sudo apt install certbot python3-certbot-nginx -y 
+echo "Adding cert for $websiteUrl " 
+echo "sudo certbot --nginx -d $websiteUrl -m $email --agree-tos -n"
+sudo certbot --nginx -d $websiteUrl -m $email --agree-tos -n
