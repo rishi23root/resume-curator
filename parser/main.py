5bc91a1e86c51ed781c3f5939e2987fb7757b7d1
Rishabh Jain <rishabhjainfinal@gmail.com>
2023-11-30 11:28:09 +0530

    started #31

---
+++ parser/main.py
@@ -0,0 +1,9 @@
+# load the file and read the file
+# extract just text from the pdf file
+# load a pre-trained model to parse the data and return in JSON format (expecting all the fields to be present, all unprocessed fields are left empty)
+# if not (3) then fill the JSON with the remaining missing fields
+# return the data
+import os
+
+# read a pdf file from the output folder
+
