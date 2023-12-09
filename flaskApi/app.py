8bb15682c21547f29be7eb084e9260bb5843d45c
Rishabh Jain <rishabhjainfinal@gmail.com>
2023-12-09 12:27:57 +0530

    implemented #23 with success,need work

--- flaskApi/app.py
+++ flaskApi/app.py
@@ -1,10 +1,28 @@
 import logging
-
+import os
 from flask import Flask
 from flask_cors import CORS
+from flask_restful import Api
+from flask_swagger_ui import get_swaggerui_blueprint
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 CORS(app)
+
+# swagger code
+api = Api(app)
+
+SWAGGER_URL = '/swagger'
+API_URL = "/static/swagger.json"
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={'BYR': "BYR api"}
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+
+# adding the logger to the app
 logging.basicConfig(filename='./logs/record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s : %(message)s')
 
 
